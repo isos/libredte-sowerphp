df714186e5425dd117c409cd237cedcad6666072
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-01-03 15:04:55 -0300

    fix link al usar acción listar en emitidos y recibidos

--- View/DteEmitidos/listar.php
+++ View/DteEmitidos/listar.php
@@ -2,8 +2,8 @@
 <p>Aquí podrá consultar todos los documentos emitidos por la empresa <?=$Emisor->razon_social?>.</p>
 <?php
 foreach ($documentos as &$d) {
-    $acciones = '<a href="dte_emitidos/ver/'.$d['dte'].'/'.$d['folio'].'" title="Ver DTE"><span class="fa fa-search btn btn-default"></span></a>';
-    $acciones .= ' <a href="dte_emitidos/pdf/'.$d['dte'].'/'.$d['folio'].'" title="Descargar PDF del DTE"><span class="fa fa-file-pdf-o btn btn-default"></span></a>';
+    $acciones = '<a href="ver/'.$d['dte'].'/'.$d['folio'].'" title="Ver DTE"><span class="fa fa-search btn btn-default"></span></a>';
+    $acciones .= ' <a href="pdf/'.$d['dte'].'/'.$d['folio'].'" title="Descargar PDF del DTE"><span class="fa fa-file-pdf-o btn btn-default"></span></a>';
     $d[] = $acciones;
     $d['total'] = num($d['total']);
     unset($d['dte']);
--- View/DteRecibidos/listar.php
+++ View/DteRecibidos/listar.php
@@ -2,7 +2,7 @@
 <p>Aquí podrá consultar todos los documentos recibidos por la empresa <?=$Emisor->razon_social?>.</p>
 
 <div class="text-right">
-    <a href="dte_recibidos/agregar" class="btn btn-default">
+    <a href="agregar" class="btn btn-default">
         <span class="fa fa-plus"></span>
         Agregar DTE recibido
     </a>
@@ -11,8 +11,8 @@
 
 <?php
 foreach ($documentos as &$d) {
-    $acciones = '<a href="dte_recibidos/modificar/'.$d['emisor'].'/'.$d['dte'].'/'.$d['folio'].'" title="Modificar DTE" class="btn btn-default'.($d['intercambio']?' disabled':'').'"><span class="fa fa-edit"></span></a>';
-    $acciones .= ' <a href="dte_intercambios/pdf/'.$d['intercambio'].'" title="Descargar PDF del DTE" class="btn btn-default'.(!$d['intercambio']?' disabled':'').'" role="button"><span class="fa fa-file-pdf-o"></span></a>';
+    $acciones = '<a href="modificar/'.$d['emisor'].'/'.$d['dte'].'/'.$d['folio'].'" title="Modificar DTE" class="btn btn-default'.($d['intercambio']?' disabled':'').'"><span class="fa fa-edit"></span></a>';
+    $acciones .= ' <a href="../dte_intercambios/pdf/'.$d['intercambio'].'" title="Descargar PDF del DTE" class="btn btn-default'.(!$d['intercambio']?' disabled':'').'" role="button"><span class="fa fa-file-pdf-o"></span></a>';
     $d[] = $acciones;
     $d['total'] = num($d['total']);
     unset($d['emisor'], $d['dte']);
