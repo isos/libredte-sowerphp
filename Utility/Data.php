3595210df5b9b2c9953037fa12950b3f9e2b9e58
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-01-26 00:06:19 -0300

    Se usa clase Utility_Data de sowerphp\core

--- Utility/Data.php
+++ Utility/Data.php
@@ -30,7 +30,7 @@ namespace website\Dte;
  * Requiere php5-mcrypt
  *
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
- * @version 2015-09-19
+ * @version 2016-01-26
  */
 class Utility_Data
 {
@@ -38,43 +38,29 @@ class Utility_Data
     /**
      * Método que encripta un texto plano
      * @param plaintext Texto plano a encriptar
-     * @param key Índice en la configuración para obtener la clave a usar
      * @return Texto encriptado en base64
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2016-01-02
+     * @version 2016-01-26
      */
-    public static function encrypt($plaintext, $key = 'dte.pkey')
+    public static function encrypt($plaintext)
     {
-        $key = \sowerphp\core\Configure::read($key);
-        if (!$key)
-            return base64_encode($plaintext);
-        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
-        return base64_encode($iv.$ciphertext);
+        return \sowerphp\core\Utility_Data::encrypt(
+            $plaintext, \sowerphp\core\Configure::read('dte.pkey')
+        );
     }
 
     /**
      * Método que desencripta un texto encriptado
      * @param $ciphertext_base64 Texto encriptado en base64 a desencriptar
-     * @param key Índice en la configuración para obtener la clave a usar
      * @return Texto plano
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2016-01-02
+     * @version 2016-01-26
      */
-    public static function decrypt($ciphertext_base64, $key = 'dte.pkey')
+    public static function decrypt($ciphertext_base64)
     {
-        if (empty($ciphertext_base64))
-            return $ciphertext_base64;
-        $ciphertext_dec = base64_decode($ciphertext_base64);
-        $key = \sowerphp\core\Configure::read($key);
-        if (!$key)
-            return $ciphertext_dec;
-        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-        $iv_dec = substr($ciphertext_dec, 0, $iv_size);
-        $ciphertext_dec = substr($ciphertext_dec, $iv_size);
-        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-        return $plaintext_dec;
+        return \sowerphp\core\Utility_Data::decrypt(
+            $ciphertext_base64, \sowerphp\core\Configure::read('dte.pkey')
+        );
     }
 
 }
