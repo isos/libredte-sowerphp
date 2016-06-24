0069cc888b7f8a50722823ab79604c9feb49eede
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-06-24 11:11:56 -0400

    Se mueve placeholder de periodo a la observación (creaba confusión)

--- View/DteRecibidos/agregar_modificar.php
+++ View/DteRecibidos/agregar_modificar.php
@@ -98,8 +98,7 @@ echo $f->input([
     'name' => 'periodo',
     'label' => 'Período',
     'value' => isset($DteRecibido) ? $DteRecibido->periodo : '',
-    'placeholder' => \sowerphp\general\Utility_Date::nextPeriod(),
-    'help' => 'Período en el que registrar el documento, sólo si es diferente al mes de la fecha de emisión. Formato: AAAAMM',
+    'help' => 'Período en el que registrar el documento, sólo si es diferente al mes de la fecha de emisión. Formato: AAAAMM Ejemplo: '.\sowerphp\general\Utility_Date::nextPeriod(),
 ]);
 echo '</div>',"\n";
 echo '<div class="col-md-6">',"\n";
